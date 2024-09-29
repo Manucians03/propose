b1eef2b90a60b53031bedf57cf9928a2697699e1
Minh <tuanminhnguyen321@gmail.com>
2024-09-29 10:21:07 -0400

    fix assets error

---
+++ src/assets/index.ts
@@ -0,0 +1,25 @@
+import anhvatui from "./images/anhvatui.jpg";
+import landau from "./images/landau.png";
+import gokart from "./images/gokart.jpg";
+import emvahoa from "./images/emvahoa.jpg";
+import emvahoa2 from "./images/emvahoa2.jpg";
+import firstdate from "./images/firstdate.jpg";
+import locket from "./images/locket.png";
+import hoahong from "./images/hoahong.jpg";
+import kem from "./images/kem.jpg";
+import kiss from "./images/kiss.jpg";
+import lastday from "./images/lastday.jpg";
+
+export {
+    anhvatui,
+    landau,
+    gokart,
+    emvahoa,
+    emvahoa2,
+    firstdate,
+    locket,
+    hoahong,
+    kem,
+    kiss,
+    lastday,
+};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,6 +4,8 @@ import type {
   TTimeline,
 } from "../types";
 
+import * as assets from "../assets";
+
 const navLinks: TNavLink[] = [
   {
     id: "introduction",
@@ -57,67 +59,67 @@ const timelines: TTimeline[] = [
   },
   {
     name: "Lần đầu mình đi chơi",
-    photo: "/src/assets/images/landau.png",
+    photo: assets.landau,
     date: "19/07/2024",
     description: "Cuối cùng thì ngày này cũng tới moahahahaa! Ê nhưng mà mình không có ảnh hôm này huhu!!! Có mỗi ảnh cap màn hình Zalo, mình đang ngổi cafe Lâm thì bố anh gọi về nấu cơm mdr 😭",
   },
   {
     name: "Đi chơi Hà Nội tập 1",
-    photo: "/src/assets/images/gokart.jpg",
+    photo: assets.gokart,
     date: "25/07/2024",
     description: "Lần đầu anh đi date sau 1 tỉ năm. Ngày quốc tang 2 đứa đèo nhau đi chơi quanh thủ đô lol 🤪 Mình đi chơi Go Kart, đi chân cầu Long Biên, đi ăn ở phố cổ rồi đi cafe đường tàu.",
   },
   {
     name: "Our first date, according to you 🙄",
-    photo: "/src/assets/images/firstdate.jpg",
+    photo: assets.firstdate,
     date: "29/07/2024",
     description: "Lần đầu mình bike date sau mấy cái kèo mồm. Mình đạp quanh hồ An Biên xong vào cafe Vòng trú mưa phải 2 tiếng. And I ased you out. And you said yes. And we had our first kiss 👅",
   },
   {
     name: "Cuối tuần đầu ở Hải Phòng",
-    photo: "/src/assets/images/locket.png",
+    photo: assets.locket,
     date: "04-05/08/2024",
     description: "Tình mới chớm nở đã phải mập mờ xa 👎 Anh đi làm cả tuần cuối tuần mới về Hải Phòng thăm em gái quê được. Mình đi Arta nè, lượn phố nè, đạp xe qua cầu Hoàng Văn Thụ nè, xong đi work date lần 1, mở đầu cho 1 chuỗi work date mỗi chiều thứ 2. This Locket pic hints được 1 số thằng.",
   },
   {
     name: "Đi chơi Hà Nội tập 2",
-    photo: "/src/assets/images/emvahoa.jpg",
+    photo: assets.emvahoa,
     date: "07-10/08/2024",
     description: "Buổi date hôm thứ 4 thành công ác: mình đi ăn ramen, đi làm nến rồi đi Bình Minh Jazz Club. On Friday we cooked, we cleaned, I teached, we slept and we discovered our talent for cuddling. And I got you the first bouquet of flowers. Date xong em đi Ba Vì còn anh đi gặp chồng yêu lần 2 hihi 🍭",
   },
   {
     name: "Cuối tuần thứ 2 ở Hải Phòng",
-    photo: "/src/assets/images/emvahoa2.jpg",
+    photo: assets.emvahoa2,
     date: "11-12/08/2024",
     description: "Another weekend, another bike date, another bunch of flowers, and another unproductive Monday work date 🤦‍♂️ ",
   },
   {
     name: "1 cuối tuần nữa ở Hải Phòng",
-    photo: "/src/assets/images/anhvatui.jpg",
+    photo: assets.anhvatui,
     date: "17-18/08/2024",
     description: "Cuối tuần này minh đi đồi Thiên Văn nè. Trên đường về được đón bão nhẹ nữa.",
   },
   {
     name: "Đi chơi Hà Nội tập 3 (mong chưa phải tập cuối)",
-    photo: "/src/assets/images/kiss.jpg",
+    photo: assets.kiss,
     date: "22-24/08/2024",
     description: "Đi thrift đi ăn đi chạy bão to đi bar đi Nậu đi cafe đi ấppp 🙌 Xong phim em đi làng cổ Đường Lâm anh đi Hạ Long.",
   },
   {
     name: "Thử thách date 7 ngày 1 tuần",
-    photo: "/src/assets/images/kem.jpg",
+    photo: assets.kem,
     date: "26/08-01/09/2024",
     description: "Khởi nguồn của chuỗi date ma quỷ, 1 tuần đi 10 cái date anh không liệt kê nổi nữa... Trong đó có hôm thứ 6 mình sáng đi thrift, chiều đi lấy cao răng, xong tối đi đua xe. Anh nghỉ làm sớm 1 tuần để về quê healing ai ngờ phải đi date full time.",
   },
   {
     name: "Anh cút khỏi Việt Nam 🛫",
-    photo: "/src/assets/images/lastday.jpg",
+    photo: assets.lastday,
     date: "02/09/2024",
     description: "Ngày này xứng đáng đi vào lịch sử 🙌 Tưởng đi lượn phố buổi sáng chiu chiu thôi ai dè... Xong anh còn kịp tặng em 1 bó hoa cuối. Xong còn kịp ăn bánh trung thu cho đúng plan ban đầu. Siêu hiệu quả Dihu ơi!",
   },
   {
     name: "Đến lượt em cút khỏi Việt Nam 🛫",
-    photo: "/src/assets/images/hoahong.jpg",
+    photo: assets.hoahong,
     date: "04/09/2024",
     description: "Nhìn hoa ngày cuối anh tặng em nè. Thế là hết dấu chân đôi mình ở Việt Nam...",
   },
